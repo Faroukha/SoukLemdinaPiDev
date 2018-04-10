b327e4a3f8bc7a346f1e90f8be3e83f03cf9f723
Ben Hassouna <nihel.benhassouna@esprit.tn>
2018-04-10 16:52:45 +0100

    Rate 50%

---
+++ src/EvenementBundle/Entity/Rating.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace EvenementBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rating
+ *
+ * @ORM\Table(name="rating")
+ * @ORM\Entity(repositoryClass="EvenementBundle\Repository\RatingRepository")
+ */
+class Rating
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="rating", type="integer", nullable=true)
+     */
+    private $rating;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idE", type="integer", nullable=true)
+     */
+    private $idE;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     *
+     * @return Rating
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return int
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set idE
+     *
+     * @param integer $idE
+     *
+     * @return Rating
+     */
+    public function setIdE($idE)
+    {
+        $this->idE = $idE;
+
+        return $this;
+    }
+
+    /**
+     * Get idE
+     *
+     * @return int
+     */
+    public function getIdE()
+    {
+        return $this->idE;
+    }
+}
+
